01846de0777e7ee50598a2787eee91fd93188982
akshatmukhriya <akshatmukhriya09@gmail.com>
2025-12-25 23:34:31 +0530

    Shifting to a complete UFO infra v20 {Final Demo}

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,18 +1,16 @@
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = module.vpc.vpc_id
+}
+
 output "cluster_name" {
-  description = "The name of the EKS cluster"
-  value       = module.eks.cluster_name
+  value = module.eks.cluster_name
 }
 
 output "region" {
-  description = "AWS region"
-  value       = "us-east-1" # Or use your var.region
-}
-
-output "karpenter_node_role_arn" {
-  description = "The ARN of the IAM role for Karpenter nodes"
-  value       = module.eks.eks_managed_node_groups["brain"].iam_role_arn
+  value = "us-east-1"
 }
 
 output "lb_controller_role_arn" {
-   value = module.lb_role.iam_role_arn
+  value = module.lb_role.iam_role_arn
 }
