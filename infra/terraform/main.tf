e16b62e2b635e79e0909334d0eaf0a4628e780c1
akshatmukhriya <akshatmukhriya09@gmail.com>
2025-12-26 15:53:46 +0530

    Spin Up for Final Demov6

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,4 +1,4 @@
-# --- NEURAL HYPERNOVA: SOVEREIGN ARCHITECTURE V1.3.0 ---
+# --- NEURAL HYPERNOVA: ATOMIC INFRASTRUCTURE ---
 
 terraform {
   required_version = ">= 1.5.0"
@@ -14,12 +14,19 @@ terraform {
 
 provider "aws" { region = "us-east-1" }
 
-variable "runner_arn" { type = string }
+variable "runner_arn" { type = string; default = "" }
 
-# --- VPC ---
+# --- 1. DATA SOURCES ---
+data "aws_caller_identity" "current" {}
+
+data "http" "lb_policy_json" {
+  url = "https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/main/docs/install/iam_policy.json"
+}
+
+# --- 2. NETWORKING ---
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "5.2.0"
+  version = "5.2.0" # Locked for precision
   name    = "hypernova-vpc"
   cidr    = "10.0.0.0/16"
   azs     = ["us-east-1a", "us-east-1b", "us-east-1c"]
@@ -30,14 +37,17 @@ module "vpc" {
   enable_nat_gateway = true
   single_nat_gateway = true 
 
-  public_subnet_tags  = { "kubernetes.io/role/elb" = 1 }
-  private_subnet_tags = { "kubernetes.io/role/internal-elb" = 1, "karpenter.sh/discovery" = "neural-hypernova" }
+  public_subnet_tags = { "kubernetes.io/role/elb" = 1 }
+  private_subnet_tags = { 
+    "kubernetes.io/role/internal-elb" = 1
+    "karpenter.sh/discovery"          = "neural-hypernova" 
+  }
 }
 
-# --- EKS 1.31 ---
+# --- 3. THE SOVEREIGN CLUSTER ---
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "20.24.0"
+  version = "20.24.0" # Locked for precision
 
   cluster_name    = "neural-hypernova"
   cluster_version = "1.31"
@@ -46,6 +56,7 @@ module "eks" {
   subnet_ids = module.vpc.private_subnets
 
   create_cloudwatch_log_group = false
+  cluster_enabled_log_types   = []
   authentication_mode         = "API_AND_CONFIG_MAP"
 
   eks_managed_node_groups = {
@@ -58,7 +69,7 @@ module "eks" {
   }
 
   access_entries = {
-    github_runner = {
+    runner = {
       principal_arn = var.runner_arn
       policy_associations = {
         admin = {
@@ -70,20 +81,52 @@ module "eks" {
   }
 }
 
-# --- IAM FOR LBC ---
-data "http" "lb_policy" { url = "https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/main/docs/install/iam_policy.json" }
-resource "aws_iam_policy" "lb_controller" { name = "AWSLoadBalancerControllerIAMPolicy"; policy = data.http.lb_policy.response_body }
+# --- 4. THE IAM PRECISION LAYER (Replacing the buggy module) ---
 
-module "lb_role" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-  role_name = "lb-controller-role-hypernova"
-  role_policy_arns = { policy = aws_iam_policy.lb_controller.arn }
-  oidc_providers = { main = { provider_arn = module.eks.oidc_provider_arn, namespace_service_accounts = ["kube-system:aws-load-balancer-controller"] } }
+# AWS Load Balancer Controller Policy
+resource "aws_iam_policy" "lb_controller" {
+  name   = "AWSLoadBalancerControllerIAMPolicy"
+  policy = data.http.lb_policy_json.response_body
+}
+
+# IAM Role with OIDC Trust Relationship
+resource "aws_iam_role" "lb_controller" {
+  name = "lb-controller-role-hypernova"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Effect = "Allow"
+      Principal = {
+        Federated = module.eks.oidc_provider_arn
+      }
+      Condition = {
+        StringEquals = {
+          "${module.eks.oidc_provider}:sub" = "system:serviceaccount:kube-system:aws-load-balancer-controller"
+        }
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lb_controller_attach" {
+  role       = aws_iam_role.lb_controller.name
+  policy_arn = aws_iam_policy.lb_controller.arn
+}
+
+# --- 5. SECURITY RULES ---
+resource "aws_security_group_rule" "ray_dash" {
+  type              = "ingress"
+  from_port         = 8265
+  to_port           = 8265
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.eks.node_security_group_id
 }
 
 # --- OUTPUTS ---
 output "cluster_name" { value = module.eks.cluster_name }
-output "region" { value = "us-east-1" }
-output "vpc_id" { value = module.vpc.vpc_id }
-output "lb_role_arn" { value = module.lb_role.iam_role_arn }
-output "vpc_cidr" { value = module.vpc.vpc_cidr_block }
+output "region"       { value = "us-east-1" }
+output "vpc_id"       { value = module.vpc.vpc_id }
+output "lb_role_arn"  { value = aws_iam_role.lb_controller.arn }
