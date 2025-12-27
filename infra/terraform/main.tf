a95cc90ae82035b31f52aa34714da803c06127bd
akshatmukhriya <akshatmukhriya09@gmail.com>
2025-12-27 17:40:53 +0530

    Spin Up for Final Demov40

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,4 +1,4 @@
-# --- NEURAL HYPERNOVA: INDUSTRIAL INFRASTRUCTURE V7.0.0 ---
+# --- NEURAL HYPERNOVA: INDUSTRIAL INFRASTRUCTURE V8.1.0 ---
 
 terraform {
   required_version = ">= 1.5.0"
@@ -14,31 +14,23 @@ terraform {
 
 provider "aws" { region = "us-east-1" }
 
-variable "runner_arn" {
-  type    = string
-  default = ""
-}
+variable "runner_arn" { type = string; default = "" }
 
 data "aws_caller_identity" "current" {}
 data "http" "lb_policy" {
   url = "https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/main/docs/install/iam_policy.json"
 }
 
-# --- 1. NETWORK ---
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.2.0"
   name    = "hypernova-vpc"
   cidr    = "10.0.0.0/16"
   azs     = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
   enable_nat_gateway = true
   single_nat_gateway = true 
-
-  # MANDATORY FOR LBC DISCOVERY
   public_subnet_tags = { "kubernetes.io/role/elb" = 1 }
   private_subnet_tags = { 
     "kubernetes.io/role/internal-elb" = 1
@@ -46,22 +38,18 @@ module "vpc" {
   }
 }
 
-# --- 2. THE BRAIN (EKS) ---
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.24.0"
-
   cluster_name    = "neural-hypernova"
   cluster_version = "1.31"
   vpc_id          = module.vpc.vpc_id
   subnet_ids      = module.vpc.private_subnets
-
   create_cloudwatch_log_group = false
   authentication_mode         = "API_AND_CONFIG_MAP"
   cluster_endpoint_public_access = true
 
   node_security_group_enable_recommended_rules = true
-
   node_security_group_additional_rules = {
     ingress_all_vpc = {
       description = "Allow all VPC internal traffic"
@@ -104,7 +92,6 @@ module "eks" {
   }
 }
 
-# --- 3. IAM FOR LBC (PRECISION IDENTITY) ---
 resource "aws_iam_policy" "lb_controller" {
   name   = "AWSLBCPolicy-Hypernova"
   policy = data.http.lb_policy.response_body
