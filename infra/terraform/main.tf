d55cf7074f1d54fcff21b536c5512711a0a1c554
akshatmukhriya <akshatmukhriya09@gmail.com>
2025-12-28 00:15:59 +0530

    Spin Up for Final Demov66

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,4 +1,4 @@
-# --- NEURAL HYPERNOVA: INDUSTRIAL INFRASTRUCTURE V24.0.0 ---
+# --- NEURAL HYPERNOVA: INDUSTRIAL INFRASTRUCTURE V25.0.0 ---
 
 terraform {
   required_version = ">= 1.5.0"
@@ -15,10 +15,7 @@ terraform {
 
 provider "aws" { region = "us-east-1" }
 
-variable "runner_arn" {
-  type    = string
-  default = ""
-}
+variable "runner_arn" { type = string; default = "" }
 
 resource "random_string" "id" {
   length  = 4
@@ -28,39 +25,34 @@ resource "random_string" "id" {
 
 data "aws_caller_identity" "current" {}
 
-# --- 1. NETWORK ---
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.2.0"
   name    = "hypernova-vpc-${random_string.id.result}"
   cidr    = "10.0.0.0/16"
   azs     = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
   enable_nat_gateway = true
   single_nat_gateway = true 
-  public_subnet_tags = { "kubernetes.io/role/elb" = 1 }
 }
 
-# --- 2. SECURITY GROUP ---
 resource "aws_security_group" "forge_sg" {
-  name_prefix = "hypernova-forge-sg-${random_string.id.result}"
+  name_prefix = "hypernova-forge-sg-"
   vpc_id      = module.vpc.vpc_id
 
   ingress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["10.0.0.0/16"]
+    cidr_blocks = ["10.0.0.0/16"] # Internal VPC
   }
 
   ingress {
     from_port   = 30265 
     to_port     = 30265
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] # Public NLB Access
   }
 
   egress {
@@ -71,28 +63,17 @@ resource "aws_security_group" "forge_sg" {
   }
 }
 
-# --- 3. EKS CLUSTER (1.31) ---
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.24.0"
-
   cluster_name    = "hypernova-${random_string.id.result}"
   cluster_version = "1.31"
   vpc_id          = module.vpc.vpc_id
   subnet_ids      = module.vpc.private_subnets
-
-  create_kms_key              = false
-  create_cloudwatch_log_group = false
-  cluster_encryption_config   = {} 
-
-  authentication_mode            = "API_AND_CONFIG_MAP"
+  authentication_mode = "API_AND_CONFIG_MAP"
   cluster_endpoint_public_access = true
-
-  # THE PRESTIGE FIX: 
-  # Disable automatic creator permissions to stop the 409 API conflict.
   enable_cluster_creator_admin_permissions = false
 
-  # Manually define the runner's access entry. Terraform now has 100% ownership.
   access_entries = {
     runner = {
       principal_arn = var.runner_arn
@@ -114,7 +95,6 @@ module "eks" {
   }
 }
 
-# --- 4. OUTPUTS ---
 output "cluster_name"    { value = module.eks.cluster_name }
 output "vpc_id"          { value = module.vpc.vpc_id }
 output "public_subnets"  { value = module.vpc.public_subnets }
