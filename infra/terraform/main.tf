e648cb9fa1c7eda9e04eb44e60b70171c202723f
akshatmukhriya <akshatmukhriya09@gmail.com>
2025-12-27 17:48:58 +0530

    Spin Up for Final Demov42

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,16 +1,10 @@
-# --- NEURAL HYPERNOVA: INDUSTRIAL INFRASTRUCTURE V8.2.0 ---
+# --- NEURAL HYPERNOVA: INDUSTRIAL INFRASTRUCTURE V9.0.0 ---
 
 terraform {
   required_version = ">= 1.5.0"
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-    http = {
-      source  = "hashicorp/http"
-      version = "~> 3.0"
-    }
+    aws = { source = "hashicorp/aws", version = "~> 5.0" }
+    http = { source = "hashicorp/http", version = "~> 3.0" }
   }
   backend "s3" {
     key    = "eks/terraform.tfstate"
@@ -18,46 +12,40 @@ terraform {
   }
 }
 
-provider "aws" {
-  region = "us-east-1"
-}
+provider "aws" { region = "us-east-1" }
 
-# --- 1. GLOBAL VARIABLES ---
 variable "runner_arn" {
   type    = string
   default = ""
 }
 
-# --- 2. DATA SOURCES ---
 data "aws_caller_identity" "current" {}
 data "http" "lb_policy" {
   url = "https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/main/docs/install/iam_policy.json"
 }
 
-# --- 3. NETWORK (VPC) ---
+# --- 1. NETWORK ---
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.2.0"
   name    = "hypernova-vpc"
   cidr    = "10.0.0.0/16"
   azs     = ["us-east-1a", "us-east-1b", "us-east-1c"]
-
+  
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
-  single_nat_gateway = true
+  single_nat_gateway = true 
 
-  public_subnet_tags = {
-    "kubernetes.io/role/elb" = 1
-  }
-  private_subnet_tags = {
+  public_subnet_tags = { "kubernetes.io/role/elb" = 1 }
+  private_subnet_tags = { 
     "kubernetes.io/role/internal-elb" = 1
-    "karpenter.sh/discovery"          = "neural-hypernova"
+    "karpenter.sh/discovery"          = "neural-hypernova" 
   }
 }
 
-# --- 4. THE BRAIN (EKS 1.31) ---
+# --- 2. THE BRAIN (EKS) ---
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.24.0"
@@ -73,21 +61,13 @@ module "eks" {
 
   node_security_group_enable_recommended_rules = true
   node_security_group_additional_rules = {
-    ingress_all_vpc = {
-      description = "Allow all VPC internal traffic"
+    ingress_vpc_all = {
+      description = "Internal VPC access"
       protocol    = "-1"
       from_port   = 0
       to_port     = 0
       type        = "ingress"
       cidr_blocks = ["10.0.0.0/16"]
-    }
-    ingress_ray = {
-      description = "Ray Dashboard Port"
-      protocol    = "tcp"
-      from_port   = 8265
-      to_port     = 8265
-      type        = "ingress"
-      cidr_blocks = ["0.0.0.0/0"]
     }
   }
 
@@ -114,7 +94,7 @@ module "eks" {
   }
 }
 
-# --- 5. IAM FOR LBC (RAW RESOURCES) ---
+# --- 3. IAM ---
 resource "aws_iam_policy" "lb_controller" {
   name   = "AWSLBCPolicy-Hypernova"
   policy = data.http.lb_policy.response_body
@@ -128,11 +108,7 @@ resource "aws_iam_role" "lb_controller" {
       Effect = "Allow"
       Principal = { Federated = module.eks.oidc_provider_arn }
       Action = "sts:AssumeRoleWithWebIdentity"
-      Condition = {
-        StringEquals = {
-          "${module.eks.oidc_provider}:sub" = "system:serviceaccount:kube-system:aws-load-balancer-controller"
-        }
-      }
+      Condition = { StringEquals = { "${module.eks.oidc_provider}:sub" = "system:serviceaccount:kube-system:aws-load-balancer-controller" }}
     }]
   })
 }
@@ -142,7 +118,6 @@ resource "aws_iam_role_policy_attachment" "lb_controller_attach" {
   role       = aws_iam_role.lb_controller.name
 }
 
-# --- 6. OUTPUTS ---
 output "cluster_name" { value = module.eks.cluster_name }
 output "region"       { value = "us-east-1" }
 output "vpc_id"       { value = module.vpc.vpc_id }
