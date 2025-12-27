8e0224ba05b2a5009a089c37c02c31fba48a57f3
akshatmukhriya <akshatmukhriya09@gmail.com>
2025-12-28 02:09:47 +0530

    Spin Up for Final Demov73

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,4 +1,4 @@
-# --- NEURAL HYPERNOVA: INDUSTRIAL INFRASTRUCTURE V31.0.0 ---
+# --- NEURAL HYPERNOVA: INDUSTRIAL INFRASTRUCTURE V32.0.0 ---
 
 terraform {
   required_version = ">= 1.5.0"
@@ -15,6 +15,7 @@ terraform {
 
 provider "aws" { region = "us-east-1" }
 
+# THE GHOST-BREAKER: New ID for every iteration
 resource "random_string" "id" {
   length  = 4
   special = false
@@ -26,31 +27,42 @@ variable "runner_arn" {
   default = ""
 }
 
+# --- 1. NETWORK ---
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.2.0"
   name    = "hypernova-vpc-${random_string.id.result}"
   cidr    = "10.0.0.0/16"
   azs     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
   enable_nat_gateway = true
   single_nat_gateway = true 
 }
 
+# --- 2. THE BRAIN (EKS 1.31) ---
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.24.0"
+
   cluster_name    = "hypernova-${random_string.id.result}"
   cluster_version = "1.31"
   vpc_id          = module.vpc.vpc_id
   subnet_ids      = module.vpc.private_subnets
-  authentication_mode = "API_AND_CONFIG_MAP"
-  enable_cluster_creator_admin_permissions = true
-  cluster_endpoint_public_access = true
+
+  # --- ATOMIC CLEANUP: REMOVE ALL ENCRYPTION/LOG BLOCKS ---
+  # By omitting 'cluster_encryption_config' and setting 'create_kms_key' to false,
+  # we bypass the module bug entirely.
   create_kms_key = false
   create_cloudwatch_log_group = false
 
+  authentication_mode                      = "API_AND_CONFIG_MAP"
+  enable_cluster_creator_admin_permissions = true
+  cluster_endpoint_public_access           = true
+
+  # Industrial Security Rules
   node_security_group_additional_rules = {
     ingress_ray = {
       description = "Ray Dashboard NodePort"
@@ -82,6 +94,7 @@ module "eks" {
   }
 }
 
+# --- 3. OUTPUTS ---
 output "cluster_name"    { value = module.eks.cluster_name }
 output "vpc_id"          { value = module.vpc.vpc_id }
 output "public_subnets"  { value = module.vpc.public_subnets }
