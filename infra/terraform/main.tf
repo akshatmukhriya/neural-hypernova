32640935394dd1c60a5d94ec2968ff4f83a69126
akshatmukhriya <akshatmukhriya09@gmail.com>
2025-12-28 05:49:34 +0530

    Spin Up for Final Demov92

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,4 +1,4 @@
-# --- NEURAL HYPERNOVA: INDUSTRIAL INFRASTRUCTURE V45.0.0 ---
+# --- NEURAL HYPERNOVA: INDUSTRIAL INFRASTRUCTURE V45.1.0 ---
 
 terraform {
   required_version = ">= 1.5.0"
@@ -13,7 +13,9 @@ terraform {
   }
 }
 
-provider "aws" { region = "us-east-1" }
+provider "aws" {
+  region = "us-east-1"
+}
 
 resource "random_string" "id" {
   length  = 4
@@ -41,7 +43,9 @@ module "vpc" {
 
   enable_nat_gateway = true
   single_nat_gateway = true 
-  private_subnet_tags = { "karpenter.sh/discovery" = "hypernova-${random_string.id.result}" } 
+  private_subnet_tags = {
+    "karpenter.sh/discovery" = "hypernova-${random_string.id.result}"
+  }
 }
 
 # --- 2. THE BRAIN (EKS 1.31) ---
@@ -54,19 +58,16 @@ module "eks" {
   vpc_id          = module.vpc.vpc_id
   subnet_ids      = module.vpc.private_subnets
 
-  # GHOST-PROOFING: Neutralize module internal bugs
   create_kms_key              = false
   create_cloudwatch_log_group = false
   authentication_mode         = "API_AND_CONFIG_MAP"
   
-  # VISIBILITY: Forced Public Endpoint
   cluster_endpoint_public_access       = true
   cluster_endpoint_private_access      = true
   cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"]
 
   enable_cluster_creator_admin_permissions = true
 
-  # CLUSTER SECURITY: Explicitly allow HTTPS from the world
   cluster_security_group_additional_rules = {
     ingress_public_443 = {
       description = "Allow HTTPS from Internet"
@@ -78,16 +79,27 @@ module "eks" {
     }
   }
 
-  node_security_group_tags = { "karpenter.sh/discovery" = "hypernova-${random_string.id.result}" }
+  node_security_group_tags = {
+    "karpenter.sh/discovery" = "hypernova-${random_string.id.result}"
+  }
 
+  # FIXED: Removed all semicolons. Strict HCL formatting applied.
   node_security_group_additional_rules = {
     ingress_ray = {
       description = "Ray Dashboard"
-      protocol = "tcp"; from_port = 30265; to_port = 30265; type = "ingress"; cidr_blocks = ["0.0.0.0/0"]
+      protocol    = "tcp"
+      from_port   = 30265
+      to_port     = 30265
+      type        = "ingress"
+      cidr_blocks = ["0.0.0.0/0"]
     }
     ingress_vpc_all = {
       description = "Internal Handshake"
-      protocol = "-1"; from_port = 0; to_port = 0; type = "ingress"; cidr_blocks = ["10.0.0.0/16"]
+      protocol    = "-1"
+      from_port   = 0
+      to_port     = 0
+      type        = "ingress"
+      cidr_blocks = ["10.0.0.0/16"]
     }
   }
 
@@ -109,7 +121,9 @@ module "karpenter_role" {
   role_name = "karpenter-ctrl-${random_string.id.result}"
   attach_karpenter_controller_policy = true
   karpenter_controller_cluster_name  = "hypernova-${random_string.id.result}"
-  karpenter_controller_node_iam_role_arns = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/KarpenterNodeRole-hypernova-${random_string.id.result}"]
+  karpenter_controller_node_iam_role_arns = [
+    "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/KarpenterNodeRole-hypernova-${random_string.id.result}"
+  ]
   oidc_providers = {
     main = {
       provider_arn = module.eks.oidc_provider_arn
@@ -124,8 +138,18 @@ resource "aws_iam_role_policy" "karpenter_extra" {
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Action = ["eks:DescribeCluster", "ec2:DescribeSubnets", "ec2:DescribeSecurityGroups", "ec2:DescribeInstances", "ec2:DescribeInstanceTypes", "ec2:DescribeInstanceTypeOfferings", "ec2:DescribeAvailabilityZones", "ssm:GetParameter"]
-      Effect = "Allow"; Resource = "*"
+      Action = [
+        "eks:DescribeCluster",
+        "ec2:DescribeSubnets",
+        "ec2:DescribeSecurityGroups",
+        "ec2:DescribeInstances",
+        "ec2:DescribeInstanceTypes",
+        "ec2:DescribeInstanceTypeOfferings",
+        "ec2:DescribeAvailabilityZones",
+        "ssm:GetParameter"
+      ]
+      Effect   = "Allow"
+      Resource = "*"
     }]
   })
 }
@@ -136,4 +160,3 @@ output "public_subnets"  { value = module.vpc.public_subnets }
 output "random_id"       { value = random_string.id.result }
 output "karpenter_role"  { value = module.karpenter_role.iam_role_arn }
 output "node_sg_id"      { value = module.eks.node_security_group_id }
-output "private_subnets" { value = module.vpc.private_subnets }
